16392d507a48b2e7743e5bac1cc77bcd7373610a
cryptopool.builders <43019051+cryptopool-builders@users.noreply.github.com>
2018-09-14 12:33:50 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This is the entry point for configuring the system.
+# Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox
+# Updated by cryptopool.builders for crypto use...
+#####################################################
+
+source /etc/functions.sh # load our functions
+source /etc/multipool.conf
+tmpdir=$PWD
+if [ ! -d $STORAGE_ROOT/coin_builder ]; then
+mkdir -p $STORAGE_ROOT/coin_builder
+sudo cp -r $tmpdir/. $STORAGE_ROOT/coin_builder
+echo '
+#!/bin/bash
+source /etc/functions.sh # load our functions
+source /etc/multipool.conf
+cd $STORAGE_ROOT/coin_builder
+bash start.sh
+cd ~
+' | sudo -E tee /usr/bin/coinbuilder >/dev/null 2>&1
+sudo chmod +x /usr/bin/coinbuilder
+fi 
+
+cd $STORAGE_ROOT/coin_builder
+source start.sh
